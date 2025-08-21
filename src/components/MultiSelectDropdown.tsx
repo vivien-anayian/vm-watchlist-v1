35bed79e675ed2fb41be857a3af1eac2ecc4e4cd
vivien-anayian <166764409+vivien-anayian@users.noreply.github.com>
2025-08-20 17:55:10 -0700

    Updated MultiSelectDropdown.tsx

--- src/components/MultiSelectDropdown.tsx
+++ src/components/MultiSelectDropdown.tsx
@@ -125,7 +125,7 @@ const VisitorConfiguration: React.FC = () => {
   };
 
   return (
-    <div className="max-w-7xl mx-auto p-6 space-y-6">
+    <div className="max-w-7xl mx-auto p-6 space-y-8">
       {/* Header */}
       <div className="flex justify-between items-center">
         <div>
